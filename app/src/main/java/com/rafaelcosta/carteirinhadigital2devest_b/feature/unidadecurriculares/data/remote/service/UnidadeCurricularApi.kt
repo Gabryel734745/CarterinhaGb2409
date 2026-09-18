8f03f4b1ad7c3e4b68fa64ed6ee703c836f6cd3e
Rafael Costa <rafaelcost@gmail.com>
2026-09-18 15:20:27 -0300

    18/09/2026

---
+++ app/src/main/java/com/rafaelcosta/carteirinhadigital2devest_b/feature/unidadecurriculares/data/remote/service/UnidadeCurricularApi.kt
@@ -0,0 +1,11 @@
+package com.rafaelcosta.carteirinhadigital2devest_b.feature.unidadecurriculares.data.remote.service
+
+import com.rafaelcosta.carteirinhadigital2devest_b.feature.unidadecurriculares.data.remote.dto.UnidadeCurricularDTO
+import com.rafaelcosta.carteirinhadigital2devest_b.feature.unidadecurriculares.domain.model.UnidadeCurricular
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+interface UnidadeCurricularApi {
+    @GET("unidades-curriculares")
+    suspend fun listarUnidadesCurriculares():List<UnidadeCurricularDTO>
+}
